7dd050b637f4d319e0e056b96b2fa93997fb718f
breno <breno.mota@al.infnet.edu.br>
2024-05-20 01:38:52 -0300

    First commit

---
+++ src/test/java/al/infnet/edu/br/springboottp2/SpringBootTp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package al.infnet.edu.br.springboottp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTp2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
